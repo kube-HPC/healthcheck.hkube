d0bd5a4970a46243be206a9c17337782445db67d
Yehiyam Livneh <yehiyam@gmail.com>
2021-05-18 13:11:47 +0300

    add base healthchecker

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const interval = require('./lib/interval.js');
 const events = require('./lib/events.js');
 const rest = require('./lib/rest');
+const baseClasses = require('./lib/baseClasses');
 
 module.exports = {
     interval,
     events,
-    rest
+    rest,
+    baseClasses
 };
--- tests/restTest.js
+++ tests/restTest.js
@@ -2,33 +2,60 @@ const chai = require('chai');
 const { expect } = chai;
 const chaiAsPromised = require('chai-as-promised');
 let chaiHttp = require('chai-http');
+const { promisify } = require('util');
+const delay = promisify(setTimeout);
 
 chai.use(chaiAsPromised);
 chai.use(chaiHttp);
 
-const { rest, events } = require('../index')
+const { rest, baseClasses } = require('../index')
 
 describe('rest', () => {
-    before(async () => {
-        await rest.init({ port: 9999 });
+    describe('api test', () => {
+
+        before(async () => {
+            await rest.init({ port: 9999 });
+        });
+        it('should throw if no path', () => {
+            expect(() => rest.start()).to.throw('path is required');
+        });
+        it('should throw if no checker', async () => {
+            expect(() => rest.start('/foo')).to.throw('checker is required');
+        });
+        it('should create path', async () => {
+            rest.start('/true', () => true, 'true')
+            const res = await chai.request(rest._app).get('/true')
+            expect(res).to.have.status(200);
+        });
+        it('should work with two paths', async () => {
+            rest.start('/true', () => true, 'true')
+            rest.start('/false', () => false, 'false')
+            const res1 = await chai.request(rest._app).get('/true')
+            expect(res1).to.have.status(200);
+            const res2 = await chai.request(rest._app).get('/false')
+            expect(res2).to.have.status(500);
+        });
+        it('should work with HealthcheckImpl', async () => {
+            const healthChecker = new baseClasses.HealthcheckImpl();
+            healthChecker.setHealthy();
+            rest.start('/HealthcheckImpl', () => healthChecker.checkHealth(500), 'true')
+            await delay(1000);
+            let res = await chai.request(rest._app).get('/HealthcheckImpl')
+            expect(res).to.have.status(500);
+            healthChecker.setHealthy();
+            await delay(200);
+            res = await chai.request(rest._app).get('/HealthcheckImpl')
+            expect(res).to.have.status(200);
+            await delay(1000);
+            res = await chai.request(rest._app).get('/HealthcheckImpl')
+            expect(res).to.have.status(500);
+        }).timeout(5000);
     });
-    it('should throw if no path', () => {
-        expect(() => rest.start()).to.throw('path is required');
-    });
-    it('should throw if no checker', async () => {
-        expect(() => rest.start('/foo')).to.throw('checker is required');
-    });
-    it('should create path', async () => {
-        rest.start('/true', () => true, 'true')
-        const res = await chai.request(rest._app).get('/true')
-        expect(res).to.have.status(200);
-    });
-    it('should work with two paths', async () => {
-        rest.start('/true', () => true, 'true')
-        rest.start('/false', () => false, 'false')
-        const res1 = await chai.request(rest._app).get('/true')
-        expect(res1).to.have.status(200);
-        const res2 = await chai.request(rest._app).get('/false')
-        expect(res2).to.have.status(500);
+    describe('init and start tests', () => {
+        it('should create path', async () => {
+            await rest.initAndStart({ path: '/true', port: 9998 }, () => true, 'true')
+            const res = await chai.request(rest._app).get('/true')
+            expect(res).to.have.status(200);
+        });
     });
 });
